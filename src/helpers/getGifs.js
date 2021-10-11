209b1aeef07474e428ae3253b0f17e10689f698d
John Lomas <jold1992@gmail.com>
2021-10-10 20:25:59 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=ESRsLQ0gJkKY058q47fUGikJzx0x0RzQ`
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url,
+        }
+    })
+
+    return gifs;
+
+}
+
+export default getGifs
